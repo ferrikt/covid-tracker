09d7ba3fb01057e69a71d2995401550e6dc8b868
Natallia Skviarniuk <n.skviarniuk@gmail.com>
2021-03-13 20:49:50 +1100

    weal attempt to change layput

--- src/components/LeftColumn.tsx
+++ src/components/LeftColumn.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Container, GlobalCases } from "./Styled";
 
 import { useCountryDataCtx } from "../context/DataContext";
-import { Box, Grid } from "theme-ui";
+import { Box, Grid, Flex } from "theme-ui";
 
 interface IProps {
   name?: string;
@@ -21,16 +21,27 @@ const LeftColumn: React.SFC<IProps> = (props: IProps) => {
   debugger;
   return (
     <GlobalCases>
-      <div>{"Left column 3 "}</div>
-      <div>{error}</div>
+      <Grid
+        // gridArea="left"
+        //bg="black"
+        // gridTemplateRows="2fr 12fr 1.5fr"
+        gap={1}
+      >
+        <div
+        // direction="column"
+        // align="center"
+        // justify="center"
+        // p={2}
+        // bg="gray.700"
+        >
+          <div>{"Left column 3 "}</div>
+          <div>{error}</div>
 
-      <Grid gap={2} columns={[1, "1fr"]}>
-        {a.map((x) => (
-          <Box bg="muted">{x}</Box>
-        ))}
+          {a.map((x) => (
+            <Box bg="muted">{x}</Box>
+          ))}
+        </div>
       </Grid>
-
-      <ul></ul>
     </GlobalCases>
   );
 };
