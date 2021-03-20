2c226c463c88326bd03753f537628a3a37237ca3
Natallia Skviarniuk <n.skviarniuk@gmail.com>
2021-03-21 10:08:18 +1100

    add interface instead of class

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,19 @@
-export class NewsData {
-  public title: string;
-  public source: string;
-  public date: string;
-  public link: string;
-  constructor(title: string, source: string, date: string, link: string) {
-    this.title = title;
-    this.source = source;
-    this.date = date;
-    this.link = link;
-  }
+// export class NewsData {
+//   public title: string;
+//   public source: string;
+//   public date: string;
+//   public link: string;
+//   constructor(title: string, source: string, date: string, link: string) {
+//     this.title = title;
+//     this.source = source;
+//     this.date = date;
+//     this.link = link;
+//   }
+// }
+
+export interface NewsData {
+  title: string;
+  source: string;
+  date: string;
+  link: string;
 }
