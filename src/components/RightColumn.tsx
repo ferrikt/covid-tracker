3c25b52116e483f2ba7cd44f434ef82527e2a827
Natallia Skviarniuk <n.skviarniuk@gmail.com>
2021-03-23 19:54:37 +1100

    display selected country in the right column

--- src/components/RightColumn.tsx
+++ src/components/RightColumn.tsx
@@ -1,22 +1,35 @@
-import * as React from "react";
-import { Flex } from "theme-ui";
+import * as React from 'react';
+import { Flex, Grid } from 'theme-ui';
+import { useSelectCountryCtx } from '../context/selectContext';
 
 interface IProps {
-  name?: string;
+    name?: string;
 }
 
-const RightColumn: React.SFC<IProps> = (props: IProps) => (
-  <Flex
-    sx={{
-      gridArea: "right",
-      border: "1px solid #363636",
-      marginLeft: "10px",
-      background: "#222",
-      marginRight: "10px",
-    }}
-  >
-    <div>Right column 5</div>
-  </Flex>
-);
+const RightColumn: React.SFC<IProps> = (props: IProps) => {
+    const { selectedCountry } = useSelectCountryCtx();
+    return (
+        <Grid
+            sx={{
+                gridArea: 'right',
+                border: '1px solid #363636',
+                marginLeft: '10px',
+                background: '#222',
+                marginRight: '10px'
+            }}
+        >
+            <Flex
+                sx={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    background: 'gray.700'
+                }}
+            >
+                Selected country {selectedCountry}
+            </Flex>
+        </Grid>
+    );
+};
 
 export default RightColumn;
