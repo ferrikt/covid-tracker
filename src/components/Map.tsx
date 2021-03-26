16ccb405b03b00a04e1ca4a405885631c35f7ecb
Natallia Skviarniuk <n.skviarniuk@gmail.com>
2021-03-26 20:39:39 +1100

    adding a leaflet map example

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import { Grid, Flex } from 'theme-ui';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import News from './News';
 
 interface IProps {}
 
-const Map: React.SFC<IProps> = (props: IProps) => <>Its a map</>;
+const Map: React.SFC<IProps> = (props: IProps) => (
+    <>
+        <MapContainer center={[45.4, -75.7]} zoom={12} style={{ height: `100%`, width: '100%' }}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+        </MapContainer>
+    </>
+);
 
 export default Map;
